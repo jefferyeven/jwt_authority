3f88d144af9757275b375400804fe795b1d41ca4
jefferyeven <1249336792@qq.com>
2022-05-09 14:44:19 +0800

    将从header读取token下方到 token验证器上（这样就意味着我们可以使用高自由度的验证，可以使用header 存储token，使用request存储token,也可以使用cooki存储token）-完善0.1

---
+++ src/main/java/io/github/jefferyeven/jwt_authority/JwtAuthorityApplication.java
@@ -0,0 +1,13 @@
+package io.github.jefferyeven.jwt_authority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthorityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthorityApplication.class, args);
+    }
+
+}
